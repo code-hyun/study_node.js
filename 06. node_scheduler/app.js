16aba4c2cdcb526996d01aa1e10e1190c905e0ef
code-hyun <122762287+code-hyun@users.noreply.github.com>
2023-10-22 21:31:23 +0900

    node-scheduler 수정

--- 06. node_scheduler/app.js
+++ 06. node_scheduler/app.js
@@ -1,13 +1,11 @@
-const nodeschedule = require('node-schedule');
+const node_schedule = require('node-schedule');
 const fs = require('fs');
 const express = require('express');
-const SCHEDULE_PORT = 3003;
+const SCHEDULE_PORT = 3002;
 const app = express();
 const fetchModule = require('./module/fetchModule');
-const dbPostgres = require('./module/dbPostgreModule')
+const dbPostgres = require('./module/dbPostgreModule');
 const logger = require('./logger/logger');
-const { writeFileSync } = require('fs');
-
 
 let date = new Date(2023, 0, 1);  // 시작 날짜를 2023년 1월 1일로 설정
 
@@ -18,50 +16,37 @@ function formatDate(d) {
     const dd = d.getDate().toString().padStart(2, '0');
     return yyyy + mm + dd;
 }
+
+async function handleDataForDate(currentDate) {
+    let dateStr = formatDate(currentDate);
+    let basePath = `./db/${dateStr}_apiTest.json`;
+
+    if (fs.existsSync(basePath)) {
+        logger.error(`${dateStr} 이미 존재하는 파일.`);
+        currentDate.setDate(currentDate.getDate() + 1);
+        return handleDataForDate(currentDate);  // 재귀적으로 다음 날짜로 넘어간다.
+    }
+
+    logger.info(`Fetching data for date: ${dateStr}`);
+    const url = `http://openAPI.seoul.go.kr:8088/7a694f42506d796e3130347242526a72/json/DailyAverageCityAir/1/100/${dateStr}`;
+    logger.info(`날짜 : ${dateStr}`);
+    
+    try {
+        let data = await fetchModule.main(url);
+        logger.info(data);
+        fs.writeFileSync(basePath, data);
+        dbPostgres.storeToDatabase(data);
+        currentDate.setDate(currentDate.getDate() + 1);  // 작업이 성공적으로 완료되면 날짜를 증가
+    } catch (error) {
+        logger.error("요청 실패:", error.message);
+    }
+}
+
 // 스케줄러 서버
 app.listen(SCHEDULE_PORT, () => {
-    logger.info(`Scheduler listening on port ${SCHEDULE_PORT}`);
+    logger.info(`스케줄러 서버 포트 : ${SCHEDULE_PORT}`);
     // 매 1초마다 스케줄러 실행
-    nodeschedule.scheduleJob('1 * * * * *', async () => {
-
-        // yyyymmdd 형식으로 변환
-        let dateStr = formatDate(date);
-        // 공공데이터 url
-        const urlBase = 'http://openAPI.seoul.go.kr:8088/인증키/json/DailyAverageCityAir/1/100/'
-        // json으로 db 형성하기 위한 path 설정
-        let basePath = `./db/${dateStr}_apiTest.json`;
-        logger.info(`저장경로 : ${basePath}`)
-
-        while (fs.existsSync(basePath)) {
-            logger.error(`File for ${dateStr} already exists. Moving to the next date.`);
-            date.setDate(date.getDate() + 1);
-            dateStr = formatDate(date);
-            basePath = `./db/${dateStr}_apiTest.json`;
-            logger.info(basePath)
-        }
-
-        logger.info(`Fetching data for date: ${dateStr}`);
-
-        // 공공데이터 api 요청하기 위한 url 설정
-        const url = urlBase + dateStr;
-        logger.info(`날짜 : ${dateStr}`);
-    
-        try {
-            // 공공데이터 응답 데이터
-            let data = await fetchModule.main(url);
-            logger.info(data)
-
-            // 데이터를 json db로 형성
-            writeFileSync(`${basePath}`, data);
-
-            // 데이터를 DB에 저장
-            dbPostgres.storeToDatabase(data);
-
-            // 
-            date.setDate(date.getDate() + 1);  // 작업이 성공적으로 완료되면 날짜를 증가
-        } catch (error) {
-            logger.error("Failed to fetch data:", error.message);
-        }
-
+    node_schedule.scheduleJob('1 * * * * *', () => {
+        handleDataForDate(date);
     });
 });
